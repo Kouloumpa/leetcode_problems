60ff52a8573b90fb2b5f0932418adebc6d464d41
Kouloumpa <38841326+Kouloumpa@users.noreply.github.com>
2021-01-24 19:41:13 +0200

    Update 1-Two Sum.py

--- 1-Two Sum.py
+++ 1-Two Sum.py
@@ -1,5 +1,5 @@
 #Time Complexity: O(n)
-#Space Compexity: O(1)
+#Space Compexity: O(n)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         index1 = [(num , indx) for indx  , num in enumerate(nums)];
