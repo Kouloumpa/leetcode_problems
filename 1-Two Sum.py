57bc46c8d7c82c36a56df01ba57031b9b8bed4ba
Kouloumpa <38841326+Kouloumpa@users.noreply.github.com>
2021-01-24 19:18:57 +0200

    Add files via upload

---
+++ 1-Two Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        index1 = [(num , indx) for indx  , num in enumerate(nums)];
+        index1.sort
+        start , end = 0, len(nums)-1;
+        while start < end :
+            current = index1[start][0] + index1[end][0];
+            if current < target:
+                start += 1;
+            elif current > target:
+                end -=1;
+            else:
+                return [index1[start][1], index1[end][1]];
